748a7db3e9656207c152061647dde96be07d61fd
kodik01 <118074591+kodik01@users.noreply.github.com>
2025-04-29 15:06:29 +0700

    Update journal_model.py

--- journal_model.py
+++ journal_model.py
@@ -21,7 +21,7 @@ jurnal["clean_title"] = jurnal["title"].apply(clean_title)
 jurnal["clean_abstract"] = jurnal["abstract"].apply(clean_abstract)
 jurnal["combined"] = jurnal["clean_title"] + " " + jurnal["clean_abstract"]
 
-vectorizer = TfidfVectorizer(stop_words='english')
+vectorizer = TfidfVectorizer()
 tfidf_matrix = vectorizer.fit_transform(jurnal["combined"])
 
 cosine_sim_matrix = cosine_similarity(tfidf_matrix)
